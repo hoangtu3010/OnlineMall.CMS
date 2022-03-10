26fc1f77664875afec55be6076db9dde52569a51
dTeUv <hoangtu2378@gmail.com>
2022-03-10 07:53:16 +0700

    tuha update

--- constants/common.js
+++ constants/common.js
@@ -1,78 +1,4 @@
 export default {
-<<<<<<< HEAD
-    CINEMA_RANKS: [{
-            value: "normal",
-            text: "Normal",
-        },
-        {
-            value: "vip",
-            text: "VIP",
-        },
-    ],
-    CINEMA_COLUMNS: [{
-            value: 1,
-            text: "Area A",
-        },
-        {
-            value: 2,
-            text: "Area B",
-        },
-        {
-            value: 3,
-            text: "Area C",
-        },
-        {
-            value: 4,
-            text: "Area D",
-        },
-        {
-            value: 5,
-            text: "Area E",
-        },
-    ],
-    CINEMA_ROWS: [{
-            value: 1,
-            text: "Row 1",
-        },
-        {
-            value: 2,
-            text: "Row 2",
-        },
-        {
-            value: 3,
-            text: "Row 3",
-        },
-        {
-            value: 4,
-            text: "Row 4",
-        },
-        {
-            value: 5,
-            text: "Row 5",
-        },
-        {
-            value: 6,
-            text: "Row 6",
-        },
-        {
-            value: 7,
-            text: "Row 7",
-        },
-        {
-            value: 8,
-            text: "Row 8",
-        },
-        {
-            value: 9,
-            text: "Row 9",
-        },
-        {
-            value: 10,
-            text: "Row 10",
-        },
-    ],
-};
-=======
   CINEMA_RANKS: [
     {
       value: "normal",
@@ -148,4 +74,3 @@ export default {
     },
   ],
 };
->>>>>>> 0589aca71fc053a7139788a8f31af1f8fd02ef47
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,10 +1,32 @@
 <template>
-  <div class="dashboard-section row">
+  <div class="dashboard-section">
+    <div class="statistics-box">
+      <div class="statistics-box-item">
+        <i class="fas fa-shopping-bag" />
+        <h3>Bookings</h3>
+        <span>{{ listDataBookingsCount }}</span>
+      </div>
+      <div class="statistics-box-item">
+        <i class="fas fa-store-alt"></i>
+        <h3>Shops</h3>
+        <span>{{ listDataShops }}</span>
+      </div>
+      <div class="statistics-box-item">
+        <i class="fas fa-film"></i>
+        <h3>Movies</h3>
+        <span>{{ listDataMovies }}</span>
+      </div>
+      <div class="statistics-box-item">
+        <i class="fab fa-product-hunt"></i>
+        <h3>Products</h3>
+        <span>{{ listDataProducts }}</span>
+      </div>
+    </div>
     <highchart
       :options="chartOptions"
       :modules="['exporting']"
       :update="watchers"
-      style="width: 100%"
+      style="width: 100%; overflow: unset"
     />
   </div>
 </template>
@@ -13,10 +35,11 @@
 export default {
   data() {
     return {
+      lstBooking: [],
       caption: "Chart caption here",
       title: "Cinema sales statistics chart",
       subtitle: "2022",
-      points: [0, 8, 2, 6, 4, 5, 5, 7, 9, 10, 15, 13],
+      points: [],
       seriesColor: "",
       animationDuration: 1000,
       chartType: "",
@@ -45,10 +68,30 @@ export default {
     };
   },
   created() {
+    this.getListBooking()
     this.getDataBookings();
-    console.log(this.listDataBooking, 'data booking');
+    this.getDataShops();
+    this.getDataMovies();
+    this.getDataProducts();
+    setTimeout(() => {
+      this.lstBooking.forEach(e => {
+        this.points.push(e.total)
+      });
+    }, 1000);
   },
   computed: {
+    listDataBookingsCount() {
+      return this.$store.state.bookings.listBooking.length;
+    },
+    listDataShops() {
+      return this.$store.state.shops.listData.length;
+    },
+    listDataProducts() {
+      return this.$store.state.products.listData.length;
+    },
+    listDataMovies() {
+      return this.$store.state.movies.listMovies.length;
+    },
     chartOptions() {
       const ctx = this;
       return {
@@ -101,7 +144,6 @@ export default {
               style: {
                 color: "#000000",
               },
-              categories: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
             },
           },
         ],
@@ -135,13 +177,29 @@ export default {
         ],
       };
     },
-    listDataBooking() {
-      return this.$store.state.bookings.listBooking;
-    },
   },
   methods: {
+    getListBooking() {
+      this.$axios
+        .get(this.$api.BOOKING_GET_ALL)
+        .then((res) => {
+          this.lstBooking = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
     getDataBookings() {
       this.$store.dispatch("bookings/getListBooking");
+    },
+    getDataShops() {
+      this.$store.dispatch("shops/getListShops");
+    },
+    getDataMovies() {
+      this.$store.dispatch("movies/getListMovies");
+    },
+    getDataProducts() {
+      this.$store.dispatch("products/getListProducts");
     },
   },
 };
@@ -150,5 +208,62 @@ export default {
 <style scoped>
 .dashboard-section {
   padding: 0 50px;
+}
+
+.statistics-box {
+  display: flex;
+  justify-content: space-between;
+  justify-items: center;
+  align-self: center;
+}
+
+.statistics-box .statistics-box-item {
+  background: #ddd;
+  display: flex;
+  justify-items: center;
+  align-items: flex-end;
+  width: 100%;
+  margin: 20px 10px;
+  padding: 55px 35px;
+  border-radius: 10px;
+  position: relative;
+  cursor: pointer;
+  color: #fff;
+}
+
+.statistics-box .statistics-box-item:first-child {
+  background: #ec008c;
+}
+
+.statistics-box .statistics-box-item:nth-child(2) {
+  background: #ed1c24;
+}
+
+.statistics-box .statistics-box-item:nth-child(3) {
+  background: #fa5511;
+}
+
+.statistics-box .statistics-box-item:last-child {
+  background: #0ecab6;
+}
+
+.statistics-box .statistics-box-item:hover {
+}
+
+.statistics-box .statistics-box-item i {
+  font-size: 4rem;
+  margin-right: 15px;
+}
+
+.statistics-box .statistics-box-item h3 {
+  font-weight: 700;
+}
+
+.statistics-box .statistics-box-item span {
+  position: absolute;
+  font-size: 2rem;
+  top: 20px;
+  right: 20px;
+  font-weight: 600;
 }
 </style>
